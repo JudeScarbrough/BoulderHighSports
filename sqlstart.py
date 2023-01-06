c468e400cbc9441b7ff94ec843ad09489b5fbca2
JudeScarbrough <112294049+JudeScarbrough@users.noreply.github.com>
2023-01-06 15:37:10 -0700

    Update sqlstart.py

--- sqlstart.py
+++ sqlstart.py
@@ -1,11 +1,15 @@
 import functions
 import json
+import sqlexecute
 
 def start(data):
-
+    print("recieved: sql start")
     jdata = json.loads(data)
 
+    print("sending to functions to return query")
     sqlStatement = functions.jsonToQuery(jdata)
+    print("recieved back from functions")
 
-
-    print("sql stmt: " + sqlStatement)
+    print("sending to be queried in DB")
+    sqlexecute.executeStmt(sqlStatement)
+    
