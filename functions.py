ec64214dc4c970c61b3590fb7cd05ffac213b9e5
JudeScarbrough <112294049+JudeScarbrough@users.noreply.github.com>
2023-01-06 15:19:25 -0700

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,3 +1,6 @@
+
+
+
 def jsonToQuery(data):
     dataSQL = {}
 
@@ -10,4 +13,7 @@ def jsonToQuery(data):
             elif (data[x] == False):
                 dataSQL[x] = 0
 
-    return dataSQL
+    seql = f'''INSERT INTO `namenumsport`(`name`, `phone`, `MBH`, `MBA`, `WBH`, `WBA`, `MLH`, `MLA`, `WSH`, `WSA`, `UFH`, `UFA`) VALUES ('{dataSQL["fullname"]}','{dataSQL["phonenumber"]}','{dataSQL["MBH"]}','{dataSQL["MBA"]}','{dataSQL["WBH"]}','{dataSQL["WBA"]}','{dataSQL["MLH"]}','{dataSQL["MLA"]}','{dataSQL["WSH"]}','{dataSQL["WSA"]}','{dataSQL["UFH"]}','{dataSQL["UFA"]}')'''
+
+    return seql
+
