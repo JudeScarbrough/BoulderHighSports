dabd836c03c2d56e440d755b76b4a242fb5ac70b
JudeScarbrough <112294049+JudeScarbrough@users.noreply.github.com>
2023-01-06 09:21:56 -0700

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,19 +1,13 @@
-data = {"fullname": "Jude Scarbrough", "phonenumber": "5126623667", "MBH": True, "MBA": False, "WBH": True, "WBA": True, "MLH": True, "MLA": True, "WSH": True, "WSA": True, "UFH": True, "UFA": True,}
+def jsonToQuery(data):
+    dataSQL = {}
 
+    for x in data:
+        if (x == "fullname" or x == "phonenumber"):
+            dataSQL[x] = data[x]
+        else:
+            if (data[x] == True):
+                dataSQL[x] = 1
+            elif (data[x] == False):
+                dataSQL[x] = 0
 
-dataSQL = {}
-
-for x in data:
-    if (x == "fullname" or x == "phonenumber"):
-        dataSQL[x] = data[x]
-    else:
-        if (data[x] == True):
-            dataSQL[x] = 1
-        elif (data[x] == False):
-            dataSQL[x] = 0
-        
-    
-
-seql = f'''INSERT INTO `namenumsport`(`name`, `phone`, `MBH`, `MBA`, `WBH`, `WBA`, `MLH`, `MLA`, `WSH`, `WSA`, `UFH`, `UFA`) VALUES ('{dataSQL["fullname"]}','{dataSQL["phonenumber"]}','{dataSQL["MBH"]}','{dataSQL["MBA"]}','{dataSQL["WBH"]}','{dataSQL["WBA"]}','{dataSQL["MLH"]}','{dataSQL["MLA"]}','{dataSQL["WSH"]}','{dataSQL["WSA"]}','{dataSQL["UFH"]}','{dataSQL["UFA"]}')'''
-
-print(seql)
+    return dataSQL
